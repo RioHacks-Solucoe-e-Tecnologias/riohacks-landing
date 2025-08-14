91ea2c470a6573583bab8e200c758b8fee02edaa
Jheickson Felipe <felipesantosatm@outlook.com>
2025-08-14 15:51:19 -0300

    feat: integrate marquee and clients components into main page

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,23 +1,28 @@
 export const metadata = {
-  title: "Home - RioHacks",
-  description: "Soluções tecnológicas: desenvolvimento, IA, integração e consultoria",
+  title: "Riohacks – Soluções tecnológicas",
+  description:
+    "Na RioHacks utilizamos tecnologia de ponta e estratégias inovadoras para oferecer soluções diferenciadas. Não se limite ao comum! Conte conosco para transformar seu negócio ou projeto.",
 };
 
 import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
+import Marquee from "@/components/marquee";
+import Services from "@/components/services";
+import Clients from "@/components/clients";
 import Workflows from "@/components/workflows";
 import Features from "@/components/features";
 import Testimonials from "@/components/testimonials";
-import Cta from "@/components/cta";
-import Services from "@/components/services";
 import Contact from "@/components/contact";
+import Cta from "@/components/cta";
 
 export default function Home() {
   return (
     <>
       <PageIllustration />
       <Hero />
+      <Marquee />
       <Services />
+      <Clients />
       <Workflows />
       <Features />
       <Testimonials />
